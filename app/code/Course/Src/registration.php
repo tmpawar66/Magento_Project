8f6423acb8cd600280a31d6e8b55c36f0659fb0c
tippannari <tippanna@theretailinsights.com>
2024-04-29 18:58:56 +0530

    First Commit

---
+++ app/code/Course/Src/registration.php
@@ -0,0 +1,10 @@
+<?php
+/** course all rights reserved
+*   See COPYING.txt for license details.
+*/
+
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Course_Src',__DIR__);
+
+
+?>
